e8011f05f7e8c3454734c6ee865aa0c935b62b5c
Rick <rickinaction@gmail.com>
2019-03-20 09:40:50 +1000

    Simple terrain and simple enemy cube. Enemy using nav mesh to follow player.

---
+++ Zombie Runner Prototype/Assets/Scripts/Mover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float attackRange = 5f;
+    [SerializeField] Transform target;
+    NavMeshAgent navMeshAgent;
+    
+    private void Start() {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    
+    private void Update() {
+        float distance = Vector3.Distance(target.position, transform.position);
+        if (distance <= attackRange)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
+    }
+
+    private void OnDrawGizmosSelected() 
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, attackRange);    
+    }
+}
