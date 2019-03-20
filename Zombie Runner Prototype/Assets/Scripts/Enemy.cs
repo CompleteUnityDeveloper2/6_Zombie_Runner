d3cbe7f92d44a4564c40ffc01cbc8ab3ee153059
Rick <rickinaction@gmail.com>
2019-03-20 11:28:33 +1000

    Added health, animation hit event, player taking damage, started enemy animator with states

---
+++ Zombie Runner Prototype/Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float damage = 10f;
+
+    // add attack behaviour(s)
+    // add SFX
+
+    public void AttackHitEvent()
+    {
+        if (target == null) return;
+        target.GetComponent<Health>().TakeDamage(damage);
+    }
+}
---
+++ Zombie Runner Prototype/Assets/Scripts/Health.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 100f;
+
+    bool isDead = false;
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        hitPoints -= damage;
+        if (hitPoints <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        if (isDead) return;
+
+        isDead = true;
+        GetComponent<Animator>().SetTrigger("die");
+    }
+}
