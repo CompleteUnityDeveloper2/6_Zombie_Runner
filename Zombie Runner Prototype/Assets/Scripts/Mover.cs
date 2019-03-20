4c3fb8aa860a5b98d1614bd618eaa6a5e696a199
Rick <rickinaction@gmail.com>
2019-03-20 11:05:07 +1000

    Enemy rotates to look at player when is within stopping distance. Note: show stopping distance, then show problem of not rotating because nav mesh agent is not making it move, then show solution of using Slerp

--- Zombie Runner Prototype/Assets/Scripts/Mover.cs
+++ Zombie Runner Prototype/Assets/Scripts/Mover.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,18 +8,43 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] float attackRange = 5f;
     [SerializeField] Transform target;
+    
+    float turnSpeed = 5f;
+
     NavMeshAgent navMeshAgent;
     
     private void Start() {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
     
-    private void Update() {
+    private void Update()
+    {
         float distance = Vector3.Distance(target.position, transform.position);
+        FaceTarget();
+        MoveToAttack(distance);
+    }
+
+    private void MoveToAttack(float distance)
+    {
         if (distance <= attackRange)
         {
             navMeshAgent.SetDestination(target.position);
+            if (distance <= navMeshAgent.stoppingDistance)
+            {
+                
+                //AttackTarget();
+            }
         }
+    }
+
+    private void FaceTarget()
+    {
+        // find the direction of the target and normalise it
+        Vector3 direction = (target.position - transform.position).normalized;
+        // create a new Vector3 for the look rotation and store it in variable
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        // use Slerp - spherically interpolates between two vectors (link to docs)
+        transform.rotation = Quaternion.Slerp(transform.rotation,lookRotation,Time.deltaTime * turnSpeed);
     }
 
     private void OnDrawGizmosSelected() 
