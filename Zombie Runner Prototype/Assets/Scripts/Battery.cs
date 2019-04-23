1f2f26213b359c3f36568313e5dcbe859fa51e77
Rick <rickinaction@gmail.com>
2019-04-23 20:44:04 +1000

    Improved Bunker and terrain. Flashlight fade / shrinkage. Battery to replenish flashlight.

---
+++ Zombie Runner Prototype/Assets/Scripts/Battery.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : MonoBehaviour
+{
+    [SerializeField] float addIntensity = 1f;
+    [SerializeField] float restoreAngle = 90f;
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            // good to show that we have proper collision first
+            Debug.Log("Player collided with");
+            other.GetComponentInChildren<FlashLightSystem>().AddLightIntensity(addIntensity);
+            other.GetComponentInChildren<FlashLightSystem>().RestoreLightAngle(restoreAngle);
+            Destroy(gameObject);
+        }    
+    }
+}
---
+++ Zombie Runner Prototype/Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    Light myLight;
+    [SerializeField] float lightDecay = .1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minimumAngle = 40f;
+
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    public void AddLightIntensity(float batteryAmount)
+    {
+        myLight.intensity += batteryAmount;
+    }
+
+    public void RestoreLightAngle(float restoreAngle)
+    {
+        myLight.spotAngle = restoreAngle;
+    }
+
+    private void DecreaseLightAngle()
+    {
+        if (myLight.spotAngle <= minimumAngle) 
+        { 
+            return; 
+        }
+        else 
+        {
+            myLight.spotAngle -= angleDecay * Time.deltaTime;
+        }
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        myLight.intensity -= lightDecay * Time.deltaTime;
+    }
+
+
+}
