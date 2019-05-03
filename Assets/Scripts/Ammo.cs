9404c23050ce501d1b4410b964f05518d6fe682e
Rick <rickinaction@gmail.com>
2019-05-03 14:02:49 +1000

    working through ammo updates with Sam

--- Assets/Scripts/Ammo.cs
+++ Assets/Scripts/Ammo.cs
@@ -5,33 +5,63 @@ using UnityEngine;
 // note: ammo is current on the individuals weapons. May be better stored in weapon system.
 // rename to weapon
 
+// teaching a private class that sits in another class, which is serialized so we can implement in inspector 
 
 public class Ammo : MonoBehaviour
 {
-    [SerializeField] int[] currentAmmo = {10, 0, 0};
+    [SerializeField] AmmoSlot[] ammoSlots;
 
-    public int GetCurrentAmmo(AmmoType type)
+    [System.Serializable] // ensure Unity can display in the editor
+    
+    // class starts
+    class AmmoSlot  // this is a private class, used only in Ammo class
     {
-        return currentAmmo[(int)type];
+        public AmmoType ammoType;
+        public int ammo;  // public variable but it can only be used within Ammo because
+        // AmmoSlot is a private class
+    }
+    // class ends
+
+    public int GetCurrentAmmo(AmmoType ammoType)
+    {
+        AmmoSlot ammoSlot = GetAmmoSlot(ammoType);
+        return ammoSlot.ammo;
     }
 
-    public void ReduceAmmo(AmmoType type)
+    public void ReduceAmmo(AmmoType ammoType)
     {
-        if (GetCurrentAmmo(type) <= 0)
+        if (GetCurrentAmmo(ammoType) <= 0)
         {
             Debug.Log("Out of Ammo");
             // Play the 'no ammo' sound
             return;
-            
+
         }
         else
         {
-            currentAmmo[(int)type] --;
+            GetAmmoSlot(ammoType).ammo--;
         }
     }
 
-    public void IncreaseAmmo(AmmoType type, int ammoAmount)
+    public void IncreaseAmmo(AmmoType ammoType, int ammoAmount)
     {
-        currentAmmo[(int)type] += ammoAmount;
+        GetAmmoSlot(ammoType).ammo += ammoAmount;
+    }
+
+// goes through our slots and sees if there is a match to the type of ammo,
+// then we can returns that slot
+
+    private AmmoSlot GetAmmoSlot(AmmoType ammoType)
+    {
+        foreach (var slot in ammoSlots)  
+        {
+            if (slot.ammoType == ammoType) return slot;
+        }
+        // if we haven't given a starting value to the ammo slot (in our list) then...
+        // AmmoSlot newAmmoSlot = new AmmoSlot(); 
+        // newAmmoSlot.ammoType = ammoType;
+        // ammoSlots.Add(newAmmoSlot);
+        // return newAmmoSlot;
+        return null;
     }
 }
--- Assets/Scripts/AmmoType.cs
+++ Assets/Scripts/AmmoType.cs
@@ -1,3 +1,5 @@
+// we can make enum as a top level type, doesn't have to sit inside a class
+
 public enum AmmoType
 {
     Bullets,
