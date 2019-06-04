df8f47e3167aa0d4c001331beacd7882c5f39d8d
Rick <rickinaction@gmail.com>
2019-06-04 12:12:49 +1000

    19 Use Animation Events

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float damage = 40f;
+
+    void Start()
+    {
+        
+    }
+
+    public void AttackHitEvent()
+    {
+        if (target == null) return;
+        Debug.Log("bang bang");
+    }
+
+}
